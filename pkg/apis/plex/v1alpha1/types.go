cb7ad9cdc044ab550a1ce333e9e2babd8d4a8485
Matthew Dorn <madorn@gmail.com>
2018-09-08 22:37:29 -0400

    simplify types.go and and plex deploy

--- pkg/apis/plex/v1alpha1/types.go
+++ pkg/apis/plex/v1alpha1/types.go
@@ -2,26 +2,6 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/api/core/v1"
-)
-
-const (
-	// DefaultBaseImage is the default docker image to use for Plex Pods.
-	DefaultBaseImage = "plexinc/pms-docker"
-	// DefaultBaseImageVersion is the default version to use for Plex Pods.
-	DefaultBaseImageVersion = "1.13.0.5023-31d3c0c65"
-	// DefaultTimeZone is the default time zone to use for Plex Pods.
-	DefaultTimeZone = "America/New_York"
-	// DefaultClaimToken is the default claim token to use for Plex Pods.
-	DefaultClaimToken = ""
-	// DefaultConfigMapName is the default ConfigMap name to use for Plex preferences.xml
-	DefaultConfigMapName = "plex-preferences-cm"
-	// DefaultConfigMountPath is the default configuration path for Plex Pods.
-	DefaultConfigMountPath = "/config"
-	// DefaultTranscodeMountPath is the default transcode path for Plex Pods.
-	DefaultTranscodeMountPath = "/transcode"
-	// DefaultDataMountPath is the default data path for Plex Pods.
-	DefaultDataMountPath = "/data"
 )
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -44,11 +24,9 @@ type Plex struct {
 }
 // PlexSpec is the spec for the Plex Custom Resource
 type PlexSpec struct {
-	// BaseImage image to use for a Plex deployment.
-	BaseImage string `json:"baseImage"`
-
-	// BaseImageVersion is the version of base image to use.
-	BaseImageVersion string `json:"baseImageVersion"`
+	
+	// Size is the number of replicas for the Plex deployment.
+	Size int32	`json:"size"`
 
 	// TimeZone is the time zone to use.
 	TimeZone string `json:"timeZone"`
@@ -68,59 +46,10 @@ type PlexSpec struct {
 	// TranscodeMountPath path for transcode path for Plex
 	TranscodeMountPath string `json:"transcodeMountPath"`
 
-	// Pod defines the policy for pods owned by Plex operator.
-	// This field cannot be updated once the CR is created.
-	Pod *PlexPodPolicy `json:"pod,omitempty"`
 }
 
-// PlexPodPolicy defines the policy for pods owned by Plex operator.
-type PlexPodPolicy struct {
-	// Resources is the resource requirements for the plex container.
-	Resources v1.ResourceRequirements `json:"resources,omitempty"`
-}
-
-// SetDefaults sets the default vaules for the Plex spec and returns true if the spec was changed
-func (p *Plex) SetDefaults() bool {
-	changed := false
-	if len(p.Spec.BaseImage) == 0 {
-		p.Spec.BaseImage = DefaultBaseImage
-		changed = true
-	}
-	if len(p.Spec.BaseImageVersion) == 0 {
-		p.Spec.BaseImageVersion = DefaultBaseImageVersion
-		changed = true
-	}
-	if len(p.Spec.TimeZone) == 0 {
-		p.Spec.TimeZone = DefaultTimeZone
-		changed = true
-	}
-	if len(p.Spec.ClaimToken) == 0 {
-		p.Spec.ClaimToken = DefaultTimeZone
-		changed = true
-	}
-	if len(p.Spec.ConfigMapName) == 0 {
-		p.Spec.ConfigMapName = DefaultConfigMapName
-	}
-	if len(p.Spec.ConfigMountPath) == 0 {
-		p.Spec.ConfigMountPath = DefaultConfigMountPath
-		changed = true
-	}
-	if len(p.Spec.DataMountPath) == 0 {
-		p.Spec.DataMountPath = DefaultDataMountPath
-		changed = true
-	}
-	if len(p.Spec.TranscodeMountPath) == 0 {
-		p.Spec.DataMountPath = DefaultTranscodeMountPath
-		changed = true
-	}
-	return changed
-}
-
-// IsPodPolicySet shortcut function to determine Pod policy status.
-func (p *Plex) IsPodPolicySet() bool {
-	return p.Spec.Pod != nil
-}
 //PlexStatus defines the status for the Plex custom resource
 type PlexStatus struct {
-	// Fill me
+	Pods             []string          `json:"pods"`
+	ExternalAddresses map[string]string `json:"externalAddresses"`
 }
--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -1,72 +1,340 @@
 package stub
 
 import (
+	"fmt"
+	"reflect"
 	"context"
 
-	"github.com/madorn/plex-operator/pkg/apis/plex/v1alpha1"
+	v1alpha1 "github.com/madorn/plex-operator/pkg/apis/plex/v1alpha1"
 
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
-	"github.com/sirupsen/logrus"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"github.com/sirupsen/logrus"
+
+
+
 )
 
+
+
+
+
+
+
+//NewHandler returns the Handler type
 func NewHandler() sdk.Handler {
 	return &Handler{}
 }
 
+//Handler is an empty struct
 type Handler struct {
-	// Fill me
 }
 
+//Handle function is where our event logic lives
 func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 	switch o := event.Object.(type) {
-	case *v1alpha1.App:
+	case *v1alpha1.Plex:
+		plex := o
 
-	case *corev1.Service
+		// Ignore the delete event since the garbage collector will clean up all secondary resources for the CR
+		// All secondary resources must have the CR set as their OwnerReference for this to be the case
+		if event.Deleted {
+			return nil
+		}
 
+		// Create the deployment if it doesn't exist
+		dep := deploymentForPlex(plex)
+		err := sdk.Create(dep)
+		if err != nil && !apierrors.IsAlreadyExists(err) {
+			return fmt.Errorf("failed to create deployment: %v", err)
+		}
 
-		err := sdk.Create(newbusyBoxPod(o))
-		if err != nil && !errors.IsAlreadyExists(err) {
-			logrus.Errorf("Failed to create busybox pod : %v", err)
-			return err
+		// Ensure the deployment size is the same as the spec
+		err = sdk.Get(dep)
+		if err != nil {
+			return fmt.Errorf("failed to get deployment: %v", err)
+		}
+		size := plex.Spec.Size
+		if *dep.Spec.Replicas != size {
+			dep.Spec.Replicas = &size
+			err = sdk.Update(dep)
+			if err != nil {
+				return fmt.Errorf("failed to update deployment: %v", err)
+			}
+		}
+
+		// Update the Plex status with the pod names
+		podList := podList()
+		labelSelector := labels.SelectorFromSet(labelsForPlex(plex.Name)).String()
+		listOps := &metav1.ListOptions{LabelSelector: labelSelector}
+		err = sdk.List(plex.Namespace, podList, sdk.WithListOptions(listOps))
+		if err != nil {
+			return fmt.Errorf("failed to list pods: %v", err)
+		}
+		podNames := getPodNames(podList.Items)
+		if !reflect.DeepEqual(podNames, plex.Status.Pods) {
+			plex.Status.Pods = podNames
+			err := sdk.Update(plex)
+			if err != nil {
+				return fmt.Errorf("failed to update memcached status: %v", err)
+			}
 		}
 	}
 	return nil
 }
 
-// newbusyBoxPod demonstrates how to create a busybox pod
-func newbusyBoxPod(cr *v1alpha1.Plex) *corev1.Pod {
-	labels := map[string]string{
-		"app": "busy-box",
+// deploymentForPlex returns a Plex Deployment object
+func deploymentForPlex(p *v1alpha1.Plex) *appsv1.Deployment {
+	ls := labelsForPlex(p.Name)
+	replicas := p.Spec.Size
+
+	dep := &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apps/v1",
+			Kind:       "Deployment",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      p.Name,
+			Namespace: p.Namespace,
+		},
+
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+
+				Spec: v1.PodSpec{
+					ServiceAccountName:	"useroot",
+					Containers: []v1.Container{{
+						Name: "plex",
+						Resources: v1.ResourceRequirements{
+							Requests: v1.ResourceList{
+								"cpu":	*resource.NewMilliQuantity(250, resource.BinarySI),
+								"memory": *resource.NewMilliQuantity(64, resource.BinarySI),
+							},
+							Limits: v1.ResourceList{
+								"cpu":	*resource.NewMilliQuantity(500, resource.BinarySI),
+								"memory": *resource.NewMilliQuantity(128, resource.BinarySI),
+							},
+						},
+						Image:   "plexinc/pms-docker:1.13.0.5023-31d3c0c65",
+						ImagePullPolicy: v1.PullPolicy("Always"),
+						VolumeMounts:	[]v1.VolumeMount{{
+							Name: "plex-config",
+							MountPath: p.Spec.ConfigMountPath,
+						},
+						{	
+							Name: "plex-transcode",
+							MountPath: p.Spec.TranscodeMountPath,
+						},
+						{
+							Name: "plex-data",
+							MountPath: p.Spec.DataMountPath,
+						},
+					},
+						Ports:	[]v1.ContainerPort{{
+								Name:	"plex-ui",
+								Protocol:	v1.ProtocolTCP,
+								ContainerPort:	32400,
+							},
+							{
+								Name:	"plex-home",
+								Protocol:	v1.ProtocolTCP,
+								ContainerPort: 3005,
+							},
+							{
+								Name:	"plex-roku",
+								Protocol:	v1.ProtocolTCP,
+								ContainerPort: 8324,
+							},
+							{
+								Name:	"plex-dlna-tcp",
+								Protocol:	v1.ProtocolTCP,
+								ContainerPort: 32469,
+							},
+							{
+								Name: "plex-dlna-udp",
+								Protocol:	v1.ProtocolUDP,
+								ContainerPort: 1900,
+							},
+							{
+								Name: "plex-discovery1",
+								Protocol:	v1.ProtocolUDP,
+								ContainerPort: 32410,
+							},
+							{
+								Name: "plex-discovery2",
+								Protocol: v1.ProtocolUDP,
+								ContainerPort: 32412,
+							},
+							{
+								Name: "plex-discovery3",
+								Protocol:	v1.ProtocolUDP,
+								ContainerPort: 32413,
+							},
+							{
+								Name: "plex-discovery4",
+								Protocol: v1.ProtocolUDP,
+								ContainerPort: 32414,
+							},
+						},
+						Env:	[]v1.EnvVar{{
+								Name: "TZ",
+								Value: p.Spec.TimeZone,
+							},
+							{
+								Name: "CLAIM_TOKEN",
+								Value: p.Spec.ClaimToken,
+							},
+						},
+						ReadinessProbe: &v1.Probe{
+							Handler: v1.Handler{
+								Exec: &v1.ExecAction{
+									Command: []string{
+										"/bin/sh",
+										"-c",
+										"LD_LIBRARY_PATH=/usr/lib/plexmediaserver '/usr/lib/plexmediaserver/Plex Media Server Tests' --gtest_filter=SanityChecks",
+									},
+								},
+							},
+								InitialDelaySeconds: 5,
+								PeriodSeconds: 10,
+							},
+						LivenessProbe: &v1.Probe{
+							Handler: v1.Handler{
+									HTTPGet: &v1.HTTPGetAction{
+										Path: "/",
+										Port: intstr.FromInt(32400),	
+										Scheme:	v1.URISchemeHTTP,
+									},
+								},
+								InitialDelaySeconds: 15,
+								PeriodSeconds:       20,
+								},
+							}},
+					InitContainers: []v1.Container{{
+						Name: "init",
+						Resources: v1.ResourceRequirements{
+							Requests: v1.ResourceList{
+								"cpu":	*resource.NewMilliQuantity(250, resource.BinarySI),
+								"memory": *resource.NewMilliQuantity(64, resource.BinarySI),
+							},
+							Limits: v1.ResourceList{
+								"cpu":	*resource.NewMilliQuantity(500, resource.BinarySI),
+								"memory": *resource.NewMilliQuantity(128, resource.BinarySI),
+							},
+						},
+						Image:   "busybox:1.29",
+						ImagePullPolicy: v1.PullPolicy("Always"),
+						Command:	[]string{
+							"/bin/sh",
+							"-c",
+							"mkdir -p /config/Library/Application Support/Plex Media Server && cp /etc/plex/Preferences.xml /config/Library/Application Support/Plex Media Server",
+						},
+						VolumeMounts:	[]v1.VolumeMount{{
+							Name: "plex-config",
+							MountPath: p.Spec.ConfigMountPath,
+						},
+						{	
+							Name: "plex-preferences",
+							MountPath: "/etc/plex",
+						},
+					},
+				}},
+					Volumes:	[]v1.Volume{{
+						Name:	"plex-config",
+						VolumeSource:	v1.VolumeSource{
+								PersistentVolumeClaim:	&v1.PersistentVolumeClaimVolumeSource{
+									ClaimName:	"plex-config",
+								},
+							},
+						},
+						{
+						Name: "plex-transcode",
+						VolumeSource: v1.VolumeSource{
+								PersistentVolumeClaim: &v1.PersistentVolumeClaimVolumeSource{
+									ClaimName: "plex-transcode",
+								},
+							},
+						},
+						{
+						Name: "plex-data",
+						VolumeSource:	v1.VolumeSource{
+								PersistentVolumeClaim:	&v1.PersistentVolumeClaimVolumeSource{
+									ClaimName:	"plex-data",
+								},
+							},
+						},
+						{
+						Name: "plex-preferences",
+						VolumeSource:	v1.VolumeSource{
+								ConfigMap:	&v1.ConfigMapVolumeSource{
+									LocalObjectReference:	v1.LocalObjectReference{
+										Name: p.Spec.ConfigMapName,
+									},
+						},
+					},
+				},
+			},
+		},
+	},
+},
 	}
-	return &corev1.Pod{
+addOwnerRefToObject(dep, asOwner(p))
+	return dep
+}
+
+// labelsForPlex returns the labels for selecting the resources
+// belonging to the given memcached CR name.
+func labelsForPlex(name string) map[string]string {
+	return map[string]string{"app": "plex", "tier": "frontend", "environment": "prod"}
+	}
+
+// addOwnerRefToObject appends the desired OwnerReference to the object
+func addOwnerRefToObject(obj metav1.Object, ownerRef metav1.OwnerReference) {
+	obj.SetOwnerReferences(append(obj.GetOwnerReferences(), ownerRef))
+}
+
+// asOwner returns an OwnerReference set as the memcached CR
+func asOwner(p *v1alpha1.Plex) metav1.OwnerReference {
+	trueVar := true
+	return metav1.OwnerReference{
+		APIVersion: p.APIVersion,
+		Kind:       p.Kind,
+		Name:       p.Name,
+		UID:        p.UID,
+		Controller: &trueVar,
+	}
+}
+
+// podList returns a v1.PodList object
+func podList() *v1.PodList {
+	return &v1.PodList{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "Pod",
 			APIVersion: "v1",
 		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "busy-box",
-			Namespace: cr.Namespace,
-			OwnerReferences: []metav1.OwnerReference{
-				*metav1.NewControllerRef(cr, schema.GroupVersionKind{
-					Group:   v1alpha1.SchemeGroupVersion.Group,
-					Version: v1alpha1.SchemeGroupVersion.Version,
-					Kind:    "Plex",
-				}),
-			},
-			Labels: labels,
-		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:    "busybox",
-					Image:   "busybox",
-					Command: []string{"sleep", "3600"},
-				},
-			},
-		},
 	}
+}
+
+// getPodNames returns the pod names of the array of pods passed in
+func getPodNames(pods []v1.Pod) []string {
+	var podNames []string
+	for _, pod := range pods {
+		podNames = append(podNames, pod.Name)
+	}
+	return podNames
 }
